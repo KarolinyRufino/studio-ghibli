042f08615351c527b196497f3fcb3433968af9c6
KarolinyRufino <karolinysantosofc@outlook.com>
2023-09-18 20:17:20 -0300

    fã site do studio ghibli para fins de estudo

---
+++ README.md
@@ -0,0 +1,5 @@
+# Studio Ghibli
+Landing Page feito com o intuito de praticar os estudos de HTML5 e CSS3.
+
+[Landing page Studio Ghibli](https://karolinyrufino.github.io/studio-ghibli/)
+![blog-studii-ghibli](https://github.com/KarolinyRufino/studio-ghibli/assets/99841661/fa0d1358-0993-4d69-9508-48b1e4d26283)
---
+++ index.js
@@ -0,0 +1,40 @@
+const slide = document.querySelectorAll('.slider');
+const btnPrev = document.querySelector('#prev-button');
+const btnNext = document.querySelector('#next-button');
+
+let currentSlide = 0;
+
+function hideSlide(){
+    slide.forEach(item => item.classList.remove('on'))
+}
+
+function showSlide() {
+    slide[currentSlide].classList.add('on');
+}
+
+function nextSlide() {
+    hideSlide();
+    if (currentSlide === slide.length -1) {
+        currentSlide = 0;
+    } else {
+        currentSlide++
+    }
+
+    showSlide();
+
+}
+function prevSlide() {
+    hideSlide();
+    if (currentSlide === 0 ) {
+        currentSlide = slide.length -1;
+    } else {
+        (currentSlide--)
+    }
+    showSlide();
+
+}
+
+btnNext.addEventListener("click", prevSlide);
+btnPrev.addEventListener("click", nextSlide);
+
+setInterval(nextSlide, 5000)
